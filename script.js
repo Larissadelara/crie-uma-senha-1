bde2ba8e9e8b49993d561d7dde0d76071cfedb2d
larisssa <140644842+Larissadelara@users.noreply.github.com>
2025-09-01 09:48:39 -0300

    d

---
+++ script.js
@@ -0,0 +1,8 @@
+const numeroSenha = document.querySelector('.parametro-senha-texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const checkbox  = document.querySelectorAll('.checkbox');
+const botoes = document.querySelectorAll('.parametro-senha-botao');
+const forcaSenha = document.querySelector('.forca');
+botoes[0].onclik =diminuiTamanho;
+botoes[1].onclik =aumentaTamanho;
